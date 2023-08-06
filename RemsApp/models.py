7cab6562760826ac55487bebea56ffb1ddb431c8
Anantha Teja Dasari <111896520+AnanthaTeja@users.noreply.github.com>
2023-08-06 08:54:55 +0530

    Add files via upload

--- RemsApp/models.py
+++ RemsApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 # Create your models here.
+class User(AbstractUser):
+    g = [
+        ("0", "--- Select Your Gender ---"),
+        ("1", "Male"),
+        ("2", "Female"),
+    ]
+    c = [
+        ("0", "Select your role"),
+        ("1", "Buyer"),
+        ("2", "Seller"),
+    ]
+    mble = models.CharField(max_length=10, null=True, blank=True)
+    gdr = models.CharField(choices=g, default="0", max_length=5)
+    role_type = models.CharField(choices=c, default="0", max_length=5)
+    eid = models.CharField(max_length=10)
+    pfimg = models.ImageField(upload_to="Profiles/", default="profile.png")
+    groups = models.ManyToManyField(Group, related_name="custom_users")
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_users")
--- RemsApp/views.py
+++ RemsApp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import NwForm
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    return render(request, "html/home.html")
+
+
+def about(request):
+    return render(request, "html/about.html")
+
+
+def contact(request):
+    return render(request, "html/contact.html")
+
+
+def register(request):
+    if request.method == "POST":
+        f = NwForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, "User Created Successfully")
+            return redirect("/lgn")
+    else:
+        f = NwForm()
+    return render(request, "html/register.html")
